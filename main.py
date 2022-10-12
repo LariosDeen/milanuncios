9424414750e907c78e609290b9d6e6fb9463aea5
Dimitri Larios <lariosdimitri@yandex.ru>
2022-10-12 15:21:29 +0300

    prises_list and visualization functions created

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 from datetime import date
 
 from openpyxl import load_workbook
 from openpyxl.styles import Border, Side, Font, PatternFill, Alignment
 from requests_html import HTMLSession
+
+START_PRICE = 600
 
 
 class Blocked(Exception):
@@ -29,45 +32,53 @@ def get_number(lnk):
         return 0
 
 
-zero = lambda x: '' if x >= 10 else '0'
+def prises_list(start_price):
+    """List of twains of price forks."""
+    lst = []
+    for i in range(0, 301, 100):
+        twain = (str(start_price + i), str(start_price + i + 99))
+        lst.append(twain)
+    return lst
+
+
+def visualization(count):
+    """Visualization of the script process."""
+    zero = lambda x: '' if x >= 10 else '0'
+    num_dict = {1: 'st', 2: 'nd', 3: 'rd'}
+    empty_space = 10 - count
+    if count < 4:
+        suffix = num_dict[count]
+    else:
+        suffix = 'th'
+    return f'Parsing of {zero(count)}{count}-{suffix} page' + ' ' * empty_space + ' ...'
+
+
 towns = ('malaga', 'torremolinos')
-# price ranges
-prises = (('600', '699'), ('700', '799'), ('800', '899'), ('900', '999'))
-lst = []
+final_list = []
 session = HTMLSession()
 counter = 0
-empty_space = 10
 
 for town in towns:
-    for prise_from, prise_to in prises:
+    for prise_from, prise_to in prises_list(START_PRICE):
         link = f'https://www.milanuncios.com/alquiler-de-pisos-en-{town}-malaga/?' \
                f'desde={prise_from}&hasta={prise_to}&demanda=n&banosd=2&dormd=3'
-        # detect the number of objects
         number = get_number(link)
-        lst.append(number)
-        # visualization of the script process
+        final_list.append(number)
         counter += 1
-        empty_space -= 1
-        numb = {1: 'st', 2: 'nd', 3: 'rd'}
-        if counter < 4:
-            suffix = numb[counter]
-        else:
-            suffix = 'th'
-        print(f'Parsing of {zero(counter)}{counter}-{suffix} page' + ' ' * empty_space + ' ...')
+        print(visualization(counter))
 
 for town in towns:
     link = f'https://www.milanuncios.com/alquiler-de-pisos-en-{town}-malaga/?demanda=n&banosd=1&dormd=4'
     number = get_number(link)
-    lst.append(number)
-    # visualization of the software process
+    final_list.append(number)
     counter += 1
-    empty_space -= 1
-    print(f'Parsing of {zero(counter)}{counter}-th page' + ' ' * empty_space + ' ...')
+    print(visualization(counter))
 
-print(lst)
+print(final_list)
 
 # working with file Estadistica.xlsm
-book = load_workbook(filename='Estadistica.xlsm', read_only=False, keep_vba=True)
+book = load_workbook(filename='Estadistica.xlsm', read_only=False,
+                     keep_vba=True)
 sheet = book.active
 # next_row - a string to be added to the end of an existing table (Estadistica.xlsm)
 next_row = sheet.max_row + 1
@@ -80,19 +91,23 @@ medium_border = Border(left=Side(style='medium'),
 # if the date of the first cell of the last row of the active sheet of the file
 # Estadistica.xlsm is different from the current date
 if str(sheet[sheet.max_row][0].value)[0:10] != str(date.today()):
-    # add the current date to the beginning of the list lst
-    lst.insert(0, date.today())
+    # add the current date to the beginning of the list final_list
+    final_list.insert(0, date.today())
     # add a new line to the active sheet of the Estadistica.xlsm file after the last one
-    sheet.append(lst)
+    sheet.append(final_list)
     # set the formatting of the added line in the active sheet of the Estadistica.xlsm file
     sheet.row_dimensions[next_row].height = 23.6
-    sheet[next_row][0].fill = PatternFill(fill_type='solid', start_color='FFB6FCC5')
-    sheet[next_row][9].fill = PatternFill(fill_type='solid', start_color='FFF2F2F2')
-    sheet[next_row][10].fill = PatternFill(fill_type='solid', start_color='FFF2F2F2')
+    sheet[next_row][0].fill = PatternFill(fill_type='solid',
+                                          start_color='FFB6FCC5')
+    sheet[next_row][9].fill = PatternFill(fill_type='solid',
+                                          start_color='FFF2F2F2')
+    sheet[next_row][10].fill = PatternFill(fill_type='solid',
+                                           start_color='FFF2F2F2')
     sheet[next_row][0].font = Font(size=10)
     for i in range(1, 12):
         sheet.cell(row=next_row, column=i).border = medium_border
-        sheet.cell(row=next_row, column=i).alignment = Alignment(horizontal='center', vertical='center')
+        sheet.cell(row=next_row, column=i).alignment = Alignment(
+            horizontal='center', vertical='center')
     for i in range(1, 11):
         sheet[next_row][i].font = Font(size=18)
     # set the numeric date format (column A) as in the previous cell on top
@@ -102,5 +117,4 @@ if str(sheet[sheet.max_row][0].value)[0:10] != str(date.today()):
 book.save('Estadistica.xlsm')
 
 # open the file Estadistica.xlsm for visual viewing
-import os
 os.startfile('Estadistica.xlsm')
